fa1f58f8e279abb1dd90831c203cb5c33976a352
nebesnayaa <144495357+nebesnayaa@users.noreply.github.com>
2023-12-30 14:06:08 +0200

    Add files via upload

---
+++ CalculatorModel.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+
+namespace HW12_calc
+{
+    public class CalculatorModel : INotifyPropertyChanged
+    {
+        private double _numberA;
+        private double _numberB;
+        private double _result;
+
+        public double NumberA
+        {
+            get { return _numberA; }
+            set
+            {
+                if (_numberA != value)
+                {
+                    _numberA = value;
+                    OnPropertyChanged(nameof(NumberA));
+                    UpdateResult();
+                }
+            }
+        }
+
+        public double NumberB
+        {
+            get { return _numberB; }
+            set
+            {
+                if (_numberB != value)
+                {
+                    _numberB = value;
+                    OnPropertyChanged(nameof(NumberB));
+                    UpdateResult();
+                }
+            }
+        }
+
+        public double Result
+        {
+            get { return _result; }
+            private set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    OnPropertyChanged(nameof(Result));
+                }
+            }
+        }
+
+        public void UpdateResult()
+        {
+            Result = NumberA + NumberB;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW12_calc
+{
+    public partial class MainWindow : Window
+    {
+        public CalculatorModel CalculatorModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CalculatorModel = new CalculatorModel();
+            DataContext = CalculatorModel;
+
+            PreviewLostKeyboardFocus += MainWindow_PreviewLostKeyboardFocus;
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox && textBox.Text == "0")
+            {
+                textBox.Text = "";
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "0";
+            }
+            CalculatorModel.UpdateResult();
+        }
+        private void MainWindow_PreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            CalculatorModel.UpdateResult();
+        }
+    }
+}
